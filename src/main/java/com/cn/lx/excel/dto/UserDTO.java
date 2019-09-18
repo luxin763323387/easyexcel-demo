82fbf940f5da6bb3f69f6e89c308486277f4225e
StevenLu <luxin763323387@163.com>
2019-09-18 10:49:49 +0800

    @ExcelProperty(index = 1)来定位excel获取的列

--- src/main/java/com/cn/lx/excel/dto/UserDTO.java
+++ src/main/java/com/cn/lx/excel/dto/UserDTO.java
@@ -11,8 +11,13 @@ import lombok.Data;
 @Data
 public class UserDTO extends BaseRowModel {
 
+    /**
+     * index取决与excel列的位置
+     * 强制读取第三个 这里不建议 index 和 name 同时用，要么一个对象只用index，要么一个对象只用name去匹配
+     */
     @ExcelProperty(index = 0)
     private String username;
+
 
     @ExcelProperty(index = 1)
     private String password;
